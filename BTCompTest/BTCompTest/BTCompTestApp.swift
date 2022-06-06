99c868374d5b3ce3872c9c0f5f5de4c115e1b035
BlackTea <blackteachinese@gmail.com>
2022-06-06 15:34:35 +0800

    feature:init iOS project

---
+++ BTCompTest/BTCompTest/BTCompTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BTCompTestApp.swift
+//  BTCompTest
+//
+//  Created by blacktea on 2022/6/6.
+//
+
+import SwiftUI
+
+@main
+struct BTCompTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
